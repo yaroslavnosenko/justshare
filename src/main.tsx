7560a78e0e6de91bf7d2756125a0ff7354154394
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2023-08-03 12:32:29 -0400

    Small fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { AppLayout, HomePage, ProfilePage, SavedPage } from './pages'
+import { AppLayout, HomePage, ProfilePage, SavedPage } from '@/pages'
 
 import './index.css'
 
